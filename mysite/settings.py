6b056707ee755a96374b62430073ad1ea6c42436
sofia <sofiakinsht@gmail.com>
2017-12-10 12:42:17 +0100

    Added comments

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'arvet.pythonanywhere.com']
 
+LOGIN_REDIRECT_URL = '/'
+
 
 # Application definition
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
+from django.contrib.auth import views
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
 ]
